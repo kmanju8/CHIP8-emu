cdc110658906cbed05b913125b73d2caafea1b84
Keshav Manju <keshava249@gmail.com>
2021-09-30 12:15:57 +0100

    Add basic rom loading methods and main ROM

---
+++ src/chip.rs
@@ -0,0 +1,40 @@
+// Const representing size of memory, stored as unsigned int
+// of pointer-size (dependant on computer architecture)
+const MEMORY_SIZE: usize = 4096;
+
+
+pub struct CHIP8 {
+    // create 4kB of memory
+    mem: [u8; MEMORY_SIZE]
+}
+
+// impl creates methods for a given struct
+impl CHIP8 {
+    
+    // resets memory to blank I think?
+    pub fn new() -> Self {
+        Self {
+            mem:[0, MEMORY_SIZE]
+        }
+    }
+
+    pub fn load_rom(&mut self, path: &str) -> io:: Results<()> {
+
+        // tries to open rom
+        let mut rom_file = File::open(path)?;
+        // creates vector to store rom data
+        let mut rom_data = Vec::new();
+
+        // finds size of rom; but I think it also drops
+        // the rom data stored in rom_file into rom_data.
+        // rom_data is the buffer here, in which 'self' is loaded to
+        let rom_size = rom_file.read_to_end(&mut rom_data)?;
+
+        // loads rom into memory from 0x200
+        for i in 0.. rom_size {
+            self.mem[0x200 + i] = rom_data[i]
+        }
+
+        Ok(())
+    }
+}
