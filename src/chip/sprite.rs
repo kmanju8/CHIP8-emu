be8ba528bb4bb680b25277d9be65c592051f3c23
Keshav Manju <keshava249@gmail.com>
2021-11-19 13:48:43 +0000

    General progress update (and gpg test)

--- src/chip/sprite.rs
+++ src/chip/sprite.rs
@@ -14,12 +14,18 @@ impl Screen{
     // implement collision detection in here as well I guess
     pub fn draw(x: u8, y: u8, sprite: [u8;5]){
 
+        // XOR is ^
+        self.screen[]^sprite
+        // will need to throw in modulo for screen wrap
+        // need to make sure data at array addresses is changed
+
         //at x,y, draw sprite. sprite can be array of 5 numbers.
 
     }
 
     // method to pull sprite out of memory. should this be here?
-    pub fn getsprite(){
-        
+    pub fn getsprite(i: u8, n: u8){
+        //i is starting mem location of sprite, n is no of bytes long
+
     }
 }
