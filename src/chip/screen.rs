1ecc9d145cbf309a1602fdd2c4d3877da7dba77e
Keshav Manju <keshava249@gmail.com>
2022-04-07 23:14:43 +0100

    get basic sructure of drawing to screen down

--- src/chip/screen.rs
+++ src/chip/screen.rs
@@ -1,3 +1,9 @@
+use std::time::Duration;
+
+use bitvec::prelude::*;
+use sdl2::rect::Point;
+
+static FPS: u32 = 30;
 // might keep track of pixels on screen? 64x32
 // if this can be kept persistant that'd be nice. But may make sense to move one layer
 // higher into cpu.rs or something.
@@ -19,10 +25,36 @@ impl Screen{
         let mut height: usize = 0;
         for row in sprite {
 
-            self.screen[height] = self.screen[height]^((row as u64) << (56 - x));
+            self.screen[y+height] = self.screen[y+height]^((row as u64) << (56 - x));
 
+            let mut row: u64 = self.screen[y+height];
             height += 1;
+
+            // will have screen clears in between frames, so okay to draw only ON pixels and ignore OFF
+            for i in row.view_bits_mut::<Msb0>() {
+                if *i {
+                    match canvas.draw_point(Point::new(1,1)) {
+                        Ok(()) => (),
+                        Err(e) => println!("Failed to draw point to cavas: {}", e)
+                    }
+                }
+            }
+            // canvas.draw_point(Point::new(i,i));
         }
+
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / FPS));
+
+
+        // canvas.set_draw_color(Color::WHITE);
+            // for i in 0.. 10 {
+            //     canvas.draw_point(Point::new(i,i));
+            // }
+            // canvas.present();
+            // ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / FPS));
+
+        // --------------------------Bit above shows drawing some pixels to screen
+        // should I use textures instead? 
 
         // XOR is ^
         // self.screen[]^sprite
